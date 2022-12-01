a71b97c78d78226f855c77d4e5210984c52d8b2e
Alessandro Saggese <asaggese@gmail.com>
2022-12-01 19:47:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,15 +6,9 @@ const hoursElm = document.querySelector('#hours');
 const minutesElm = document.querySelector('#minutes');
 const secondsElm = document.querySelector('#seconds');
 
-// daysElm.innerHTML = 25;
-
 // Christmas's day
 const endDate = new Date("December 25 2022");
 const endDateInMs = endDate.getTime();
-// Today in ms
-const nowInMs = new Date().getTime();
-
-const diff = endDateInMs - nowInMs;
 
 // table in ms
 const secondInMs = 1000;
@@ -22,7 +16,16 @@ const minuteInMs = 60 * secondInMs;
 const hourInMs = 60 * minuteInMs;
 const dayInMs = 24 * hourInMs;
 
-daysElm.innerHTML = Math.floor(diff / dayInMs);
-hoursElm.innerHTML = Math.floor((diff % dayInMs) / hourInMs);
-minutesElm.innerHTML = Math.floor((diff % hourInMs) / minuteInMs);
-secondsElm.innerHTML = Math.floor((diff % minuteInMs) / secondInMs);
+const counterTimer = setInterval(timer, 1000);
+
+function timer() {
+   // Today in ms
+   const nowInMs = new Date().getTime();
+   
+   const diff = endDateInMs - nowInMs;
+
+   daysElm.innerHTML = Math.floor(diff / dayInMs);
+   hoursElm.innerHTML = Math.floor((diff % dayInMs) / hourInMs);
+   minutesElm.innerHTML = Math.floor((diff % hourInMs) / minuteInMs);
+   secondsElm.innerHTML = Math.floor((diff % minuteInMs) / secondInMs);
+}
